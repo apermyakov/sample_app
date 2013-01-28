5a36c8df378a4fe88980d7ef5034dfb8e50eea64
Alexandr Permyakov <permyakov@gmail.com>
2013-01-28 16:27:38 +0400

    Finished first cut of the User model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '752d76665a2d7456e9035e2281035cdceb8efb454b6cd3260f0341c5b7de25af5a955cabf7af2a42e1ad15bfddce8750d36f5547f959da8412a01e887a6491c8'
